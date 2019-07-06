dd97fd6ad5dd4a25a88ab9d3eaf6d6d6b3cf66b0
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-07-07 04:09:26 +0900

    ✅ Add return range test

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,79 @@
-import test from 'ava';
+import test, { Macro } from 'ava';
+import semver from 'semver';
 
 import { intersect } from '../src';
+
+const validateOutputRangeMacro: Macro<[string[], string]> = (
+    t,
+    input,
+    expected,
+) => {
+    t.is(semver.validRange(intersect(...input)), semver.validRange(expected));
+};
+validateOutputRangeMacro.title = (
+    providedTitle = '',
+    input,
+    expected,
+): string =>
+    (providedTitle ? `${providedTitle} ` : '') +
+    `intersect(${input.map(v => `"${v}"`).join(', ')}) === "${expected}"`;
 
 test('intersect() returns string type value', t => {
     t.is(typeof intersect('1.0.0'), 'string');
 });
+
+/*
+ * @see https://github.com/snyamathi/semver-intersect/blob/491aebfe04cf1c7a6db89a425cd56c9af2c44902/tests/unit/semver-intersect.js#L137-L207
+ */
+test(validateOutputRangeMacro, ['4.0.0'], '4.0.0');
+test(validateOutputRangeMacro, ['^4.0.0', '^4.3.0'], '^4.3.0');
+test(validateOutputRangeMacro, ['^4.0.0', '~4.3.0'], '~4.3.0');
+test(validateOutputRangeMacro, ['^1.0.0-alpha.3', '^1.2.0'], '^1.2.0');
+test(
+    validateOutputRangeMacro,
+    ['^0.14.0-beta2', '^0.14.0-beta3'],
+    '^0.14.0-beta3',
+);
+test(
+    validateOutputRangeMacro,
+    ['^0.14.0-beta', '^0.14.0-beta4'],
+    '^0.14.0-beta4',
+);
+test(validateOutputRangeMacro, ['^1.9.0-alpha', '^1.9.0-beta'], '^1.9.0-beta');
+test(validateOutputRangeMacro, ['^1.9.0-beta', '^1.9.0-alpha'], '^1.9.0-beta');
+test(
+    validateOutputRangeMacro,
+    ['^1.9.0-alpha.1', '^1.9.0-beta.2'],
+    '^1.9.0-beta.2',
+);
+test(validateOutputRangeMacro, ['1.5.16', '^1.0.0'], '1.5.16');
+test(
+    validateOutputRangeMacro,
+    ['^4.0.0', '~4.3.89', '~4.3.24', '~4.3.63'],
+    '~4.3.89',
+);
+test(validateOutputRangeMacro, ['1.0.0 - 1.5.3'], '1.0.0 - 1.5.3');
+
+test(validateOutputRangeMacro, ['>=8.0.0 >=8.16.0'], '>=8.16.0');
+test(validateOutputRangeMacro, ['>=8.2.0 >=8.1.0 <8.3.0'], '>=8.2.0 <8.3.0');
+test(validateOutputRangeMacro, ['^8.0.0 || ^8.1.0'], '^8.0.0 || ^8.1.0');
+
+test(
+    validateOutputRangeMacro,
+    ['>=8.9.4 <9 || >=10.0.0', '^6.14.0 || ^8.10.0 || >=9.10.0'],
+    '^8.10.0 || >=10.0.0',
+);
+test(
+    validateOutputRangeMacro,
+    ['^8.15.0 || >=10.0.0', '^8.10.0 || ^10.13.0 || >=11.10.1'],
+    '^8.15.0 || ^10.13.0 || >=11.10.1',
+);
+test(
+    validateOutputRangeMacro,
+    [
+        '^8.15.1 || ^9.0.7 || ^12.0.0',
+        '8.16 || >=9.7.1 || >=11.58',
+        '0.x - 11.x || >=12.8.2',
+    ],
+    '8.16 || ^9.7.1 || ^12.8.2',
+);
