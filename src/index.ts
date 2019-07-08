d7584744aba2350d2f6cdcae64818728772107c4
sounisi5011 <sounisi5011@users.noreply.github.com>
2019-07-08 11:11:13 +0900

    🐛 Fix to catch only exception of semver.Range class

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,24 @@ import { MultiRange } from './multi-range';
 import { isIntersectRanges } from './utils';
 
 export function intersect(...ranges: string[]): string | null {
-    try {
-        const semverRangeList = ranges.map(
-            rangeStr => new semver.Range(rangeStr),
-        );
-
-        if (!isIntersectRanges(semverRangeList)) {
+    const semverRangeList = (() => {
+        try {
+            return ranges.map(rangeStr => new semver.Range(rangeStr));
+        } catch (err) {
             return null;
         }
+    })();
 
-        const intersectRange = semverRangeList
-            .map(range => new MultiRange(range.set))
-            .reduce(
-                (multiRangeA, multiRangeB) =>
-                    multiRangeA.intersect(multiRangeB),
-                new MultiRange(null),
-            );
-
-        return intersectRange.valid ? String(intersectRange) : null;
-    } catch (err) {
+    if (!semverRangeList || !isIntersectRanges(semverRangeList)) {
         return null;
     }
+
+    const intersectRange = semverRangeList
+        .map(range => new MultiRange(range.set))
+        .reduce(
+            (multiRangeA, multiRangeB) => multiRangeA.intersect(multiRangeB),
+            new MultiRange(null),
+        );
+
+    return intersectRange.valid ? String(intersectRange) : null;
 }
